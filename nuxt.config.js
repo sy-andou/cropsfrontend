b78c97b1a56e7f391b759a70dd5d8b5e58c82466
sy-andou <kekeru.the.solo@gmail.com>
2022-04-23 21:45:49 +0900

    ヘッド編集

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'EC_front',
+    title: 'Crops',
     htmlAttrs: {
       lang: 'en'
     },
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/img/logo.png' }
     ]
   },
 
