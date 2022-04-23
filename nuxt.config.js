d2ebad9e782918b0db1cc10f8493f622871d586b
sy-andou <kekeru.the.solo@gmail.com>
2022-04-23 13:46:41 +0900

    JWTAuth修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
     strategies: {
       'laravelJWT': {
         provider: 'laravel/jwt',
-        url: 'http://localhost:8000',
+        url: 'https://cropsbackend.herokuapp.com',
         token: {
           maxAge: 60 * 60
         },
